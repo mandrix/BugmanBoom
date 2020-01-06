f38c3e49d53ea19b616a74a24358f2765007f7e2
mandrix <ale.aazv@icloud.com>
2020-01-06 04:48:23 -0600

    scripts

---
+++ Assets/movill.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movill : MonoBehaviour
+{
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("putoelquelolea");
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector3 Moving = new Vector3(0, 0, 0);
+        if (Input.GetKey(KeyCode.A))
+            Moving = Vector3.left;       
+        else if (Input.GetKey(KeyCode.D))
+            Moving = Vector3.right;
+        else if (Input.GetKey(KeyCode.W))
+            Moving = Vector3.up;
+        else if (Input.GetKey(KeyCode.S))
+            Moving = Vector3.down;
+        else
+            Moving = rb.velocity * -1;
+        MovingObject(Moving);
+    }
+
+    void MovingObject(Vector3 Vector) {
+        rb.AddForce(Vector);
+    }
+}
+
