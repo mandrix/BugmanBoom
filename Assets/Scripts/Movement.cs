de0ee95fc2c4dfc66b588dd17c1d7f1af4b36a3e
mandrix <ale.aazv@icloud.com>
2020-01-06 04:54:34 -0600

    refactoring

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class movill : MonoBehaviour
+public class Movement : MonoBehaviour
 {
     public Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("putoelquelolea");
+
     }
 
     // Update is called once per frame
@@ -16,7 +16,7 @@ public class movill : MonoBehaviour
     {
         Vector3 Moving = new Vector3(0, 0, 0);
         if (Input.GetKey(KeyCode.A))
-            Moving = Vector3.left;       
+            Moving = Vector3.left;
         else if (Input.GetKey(KeyCode.D))
             Moving = Vector3.right;
         else if (Input.GetKey(KeyCode.W))
@@ -28,7 +28,8 @@ public class movill : MonoBehaviour
         MovingObject(Moving);
     }
 
-    void MovingObject(Vector3 Vector) {
+    void MovingObject(Vector3 Vector)
+    {
         rb.AddForce(Vector);
     }
 }
